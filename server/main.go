339fc01870dcb9fc8b66c24bf10a6f86fdfcaf70
Jaidev Pillarisetti <jdpillaris@gmail.com>
2024-12-02 20:40:10 -0800

    Add server log message

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/jdpillaris/neo/handlers"
@@ -8,5 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/echo", handlers.EchoMatrix)
+
+	fmt.Println("Server is running on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
