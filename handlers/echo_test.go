fac0b9ca7eee495d25ff1d890cf0704168c7e491
Jaidev Pillarisetti <jdpillaris@gmail.com>
2024-12-03 20:23:13 -0800

    Move the test helper function for creating requests

--- handlers/echo_test.go
+++ handlers/echo_test.go
@@ -1,35 +1,11 @@
 package handlers
 
 import (
-	"bytes"
-	"io"
-	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
-
-func createMultipartRequest(csvContent string) (*http.Request, string) {
-	var body bytes.Buffer
-	writer := multipart.NewWriter(&body)
-
-	part, err := writer.CreateFormFile("file", "matrix.csv")
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = io.Copy(part, strings.NewReader(csvContent))
-	if err != nil {
-		panic(err)
-	}
-
-	writer.Close()
-	req := httptest.NewRequest(http.MethodPost, "/upload", &body)
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-
-	return req, writer.FormDataContentType()
-}
 
 func TestEchoMatrix(t *testing.T) {
 	testCases := []struct {
