3608d462931ca623232379d7b6f76f34f0783c34
Jaidev Pillarisetti <jdpillaris@gmail.com>
2025-02-25 12:30:42 -0800

    Setup HTTP routes

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/jdpillaris/neo/handlers"
 )
 
 func main() {
-	http.HandleFunc("/echo", handlers.EchoMatrix)
+	setupRoutes()
+
+	fmt.Println("Server is running on port 8080...")
 	http.ListenAndServe(":8080", nil)
+}
+
+func setupRoutes() {
+	http.HandleFunc("POST /echo", handlers.EchoMatrix)
 }
